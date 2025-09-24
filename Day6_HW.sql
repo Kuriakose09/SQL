54990ec261e7e4f48a8376098bdc3c90b8664538
Kuriakose Antony <kuriakoseantony09@gmail.com>
2025-09-24 09:57:45 +0530

    Day6

--- Day6_HW.sql
+++ Day6_HW.sql
@@ -1,3 +1,4 @@
+-- day6 HW
 INSERT INTO movies (title,genre,release_year,rating,box_office) VALUES ('Inception', 'Sci-Fi', 2010, 8.8, 825),( 'The Dark Knight', 'Action', 2008, 9.0, 1004),('Interstellar', 'Sci-Fi', 2014, 8.6, 677),('Joker', 'Drama', 2019, 8.4, 1074),('Dunkirk', 'War', 2017, 7.9, 527);
 
 SELECT  title,box_office as Earnings FROM movies;
