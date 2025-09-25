74499ce384a7a1d83d4a38efd4ce0e09fc784875
Kuriakose Antony <kuriakoseantony09@gmail.com>
2025-09-25 07:42:25 +0530

    Day7 HW push

--- Day7_HW.sql
+++ Day7_HW.sql
@@ -1,3 +1,4 @@
+--Day7-HW
 INSERT INTO users VALUES (1,'Raj','Chennai',88,5,'Fitness'),(2,'Anu','Mumbai',91,NULL,'Diet'),(3,'Ravi','Chennai',78,3,'Fitness'),(4,'Meena','Delhi',82,NULL,'Diet'),(5,'Farah','Mumbai',95,4,'Fitness'),
 (6,'Kiran','Pune',70,NULL,'Yoga'),(7,'Latha','Pune',87,NULL,'Fitness');
 
